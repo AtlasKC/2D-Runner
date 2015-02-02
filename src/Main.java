4677a4450c15e317a6e22d6393304a4c0a5415c4
Atlas IND <kadenc28@gmail.com>
2015-02-02 12:30:45 -0500

    Testing Git

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+/**
+ * File: Main.java
+ * Created by Atlas IND on 2/2/15.
+ * [2014] - [2015] Rostiss Development
+ * All rights reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Rostiss Development and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Rostiss Development
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Rostiss Development.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Ello m8!");
+    }
+}
